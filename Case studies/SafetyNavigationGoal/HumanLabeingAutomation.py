56e56eb5bb12d1ace3f5bd6a08a6b0c15e218d3a
LunetYifru <50957269+LunetYifru@users.noreply.github.com>
2024-02-06 11:15:15 -0500

    Update HumanLabeingAutomation.py

--- Case studies/SafetyNavigationGoal/HumanLabeingAutomation.py
+++ Case studies/SafetyNavigationGoal/HumanLabeingAutomation.py
@@ -9,7 +9,7 @@ Created on Wed Sep 27 11:29:41 2023
 import rtamt
 import math
 import pandas as pd
-from GPyOpt_SafetyPointGoal1 import robustness,specification,signal,size
+from BOSNG import robustness,specification,signal,size
 #from InitialDataset import initial_trace
 #x_trace, y_trace = initial_trace()
 
