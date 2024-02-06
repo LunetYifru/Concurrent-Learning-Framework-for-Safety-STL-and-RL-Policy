be2bcda6dfb838dcee4793421327c4a4fd802960
LunetYifru <50957269+LunetYifru@users.noreply.github.com>
2024-02-06 11:13:27 -0500

    commit

--- Case studies/SafeVelocityHalfCheetah/HumanLabelingAutomation.py
+++ Case studies/SafeVelocityHalfCheetah/HumanLabelingAutomation.py
@@ -49,39 +49,43 @@ def human_labeling(spec,operation, p_x , v_x, cost_limit):
         if rob <0:
             p_x_anom.append([j for j in p_x[i]])
             v_x_anom.append([j for j in v_x[i]])
-    j = 0
-    i = 0
-    # alter labels based on cost limit
-    while i < len(v_x_anom): # moving under cost_limit traces to regular df
-        # Define the reference points
-        reference_points = [(3.20960)]
-        p_x_values = p_x_anom[i] # Replace with v_xour actual p_x values
-        v_x_values = v_x_anom[i]  # Replace with v_xour actual v_x values
-    
-        violated_timesteps = []
-        
-        # Iterate through the trace
-        for timestep, (p_x_an, v_x_an) in enumerate(zip(p_x_values, v_x_values)):
-            # Check the STL formula for each reference point
-            violated = any (v_x_an> v_x_ref for v_x_ref in reference_points)
-        
-            if violated:
-                violated_timesteps.append(timestep)
-      
-        # Print all time steps where the STL formula was violated
-        if violated_timesteps:
-            if len(violated_timesteps) <= cost_limit:
-                print(len(violated_timesteps))
-                p_x_reg.append(p_x_values)
-                v_x_reg.append(v_x_values)  
-                p_x_anom.pop(i)
-                v_x_anom.pop(i)  
-                i+= 1
-                j+=1
-        i+=1
-    print('final sizes p_x_reg,p_x_anom',len(p_x_reg), len(p_x_anom), 'with', j, 'change(s)')
 
-             
+            
+    if cost_limit > 0:
+        j = 0
+        i = 0 
+        # alter labels based on cost limit
+        while i < len(v_x_anom): # moving under cost_limit traces to regular df
+            # Define the reference points
+            reference_points = [(3.20960)]
+            p_x_values = p_x_anom[i] # Replace with v_xour actual p_x values
+            v_x_values = v_x_anom[i]  # Replace with v_xour actual v_x values
+        
+            violated_timesteps = []
+            
+            # Iterate through the trace
+            for timestep, (p_x_an, v_x_an) in enumerate(zip(p_x_values, v_x_values)):
+                # Check the STL formula for each reference point
+                violated = any (v_x_an> v_x_ref for v_x_ref in reference_points)
+            
+                if violated:
+                    violated_timesteps.append(timestep)
+        
+            # Print all time steps where the STL formula was violated
+            if violated_timesteps:
+                if len(violated_timesteps) <= cost_limit:
+                    print(len(violated_timesteps))
+                    p_x_reg.append(p_x_values)
+                    v_x_reg.append(v_x_values)  
+                    p_x_anom.pop(i)
+                    v_x_anom.pop(i)  
+                    i+= 1
+                    j+=1
+            i+=1
+        print('Final sizes : x_reg = ',len(x_reg),', x_anom = ', len(x_anom), 'with', j, 'change(s)')
+
+    print('Sizes:  x_reg = ',len(x_reg),', x_anom = ', len(x_anom))  
+     
     p_x_reg = pd.DataFrame(p_x_reg).T
     v_x_reg = pd.DataFrame(v_x_reg).T
     p_x_anom = pd.DataFrame(p_x_anom).T
--- Case studies/SafetyNavigationGoal/HumanLabeingAutomation.py
+++ Case studies/SafetyNavigationGoal/HumanLabeingAutomation.py
@@ -9,7 +9,7 @@ Created on Wed Sep 27 11:29:41 2023
 import rtamt
 import math
 import pandas as pd
-from GPyOpt_SafetyPointCircle1 import robustness,specification,signal,size
+from GPyOpt_SafetyPointGoal1 import robustness,specification,signal,size
 #from InitialDataset import initial_trace
 #x_trace, y_trace = initial_trace()
 
@@ -19,7 +19,8 @@ def human_labeling(spec,operation, x , y, cost_limit):
     y_reg = []    
     x_anom = []
     y_anom = []
-       
+    
+    
     for i in range(len(x.columns)):
         if operation == '>' or operation == '<' or operation == 'not':
             rob = min(robustness(signal(x, y,i), specification(spec)))
@@ -45,50 +46,53 @@ def human_labeling(spec,operation, x , y, cost_limit):
         if rob >=0:
             x_reg.append([j for j in x[i]])
             y_reg.append([j for j in y[i]])
-            print('safe')
+            print('Label : safe')
         if rob <0:
             x_anom.append([j for j in x[i]])
             y_anom.append([j for j in y[i]])
-            print('unsafe')
-    j = 0
-    i = 0
-    while i < len(x_anom): #moving under cost_limit traces to regular df
-        # Define the reference points
-        reference_points = [(1.125,1.125)]
-        x_values = x_anom[i] # Replace with your actual x values
-        y_values = y_anom[i]  # Replace with your actual y values
+            print('Label : unsafe')
     
-        violated_timesteps = []
-        
-        # Iterate through the trace
-        for timestep, (x_an, y_an) in enumerate(zip(x_values, y_values)):
-            # Check the STL formula for each reference point
-            violated = any(x_an > x_ref  or y_an> y_ref for x_ref, y_ref in reference_points)
-        
-            if violated:
-                violated_timesteps.append(timestep)
-                
-        # Print all time steps where the STL formula was violated
-        if violated_timesteps:
-            if len(violated_timesteps) <= cost_limit:
-                print(len(violated_timesteps))
-                x_reg.append(x_values)
-                y_reg.append(y_values)  
-                x_anom.pop(i)
-                y_anom.pop(i)  
-                i+= 1
-                j+=1
-        i+=1
-    print('final sizes x_reg,x_anom',len(x_reg), len(x_anom), 'with', j, 'change(s)')
+    #If cost_limit != 0 
+      
+    if cost_limit > 0:
+        j = 0
+        i = 0 
+        while i < len(x_anom): #moving traces with number of violated timesteps under "cost_limit" to regular df
+        # Define the reference points
+            reference_points = [(-0.75, 1.0), (1.0, 0.20), (-1.40, 0.70), (-0.50, -0.30), (0.25, 0.90), (0.0, -1.50),(-1.9, 1.00),(1.00, -1.00)]        
+            # Sample trace data as lists of (x, y) values
+            
+            x_values = x_anom[i] # Replace with your actual x values
+            y_values = y_anom[i]  # Replace with your actual y values
 
-             
+            violated_timesteps = []
+            
+            # Iterate through the trace
+            for timestep, (x_an, y_an) in enumerate(zip(x_values, y_values)):
+                # Check the STL formula for each reference point
+                violated = any(math.sqrt((x_an - x_ref)**2 + (y_an - y_ref)**2) < 0.4 for x_ref, y_ref in reference_points)
+                if violated:
+                    violated_timesteps.append(timestep)
+            # print(len(violated_timesteps))
+            # Print all time steps where the STL formula was violated
+            if violated_timesteps:
+                if len(violated_timesteps) <= cost_limit:
+                    print(len(violated_timesteps),'changed')
+                    x_reg.append(x_values)
+                    y_reg.append(y_values)  
+                    x_anom.pop(i)
+                    y_anom.pop(i)  
+                    i+= 1
+                    j+=1
+            i+=1
+        print('Final sizes : x_reg = ',len(x_reg),', x_anom = ', len(x_anom), 'with', j, 'change(s)')
+
+    print('Sizes:  x_reg = ',len(x_reg),', x_anom = ', len(x_anom))     
     x_reg = pd.DataFrame(x_reg).T
     y_reg = pd.DataFrame(y_reg).T
     x_anom = pd.DataFrame(x_anom).T
     y_anom = pd.DataFrame(y_anom).T
 
-    
-    
     return x_reg,y_reg, x_anom , y_anom
          
 #x_reg,y_reg,x_anom,y_anom = Human_labling('F(x>=4 & y>=4)','F',x_trace,y_trace)
